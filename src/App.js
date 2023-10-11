5ffa3f7d84b4896c97a9add4cddc4139acd57f56
Joar-Rinaldo-Roos <joar.rinaldo.roos@gmail.com>
2023-10-11 11:10:45 +0200

    added a devnavbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 import { FormControl } from "react-bootstrap";
 import ApiHandler from "./ApiHandler";
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import LandingPage from "./LandingPage";
 
 const CLIENT_ID = "41a89822d42c452fb778e429576a972b";
@@ -12,8 +12,26 @@ const CLIENT_SECRET = "40a6ddb0f73d480094f24bd837e3dfba";
 export default function App() {
   return (
     <div className="container">
+      <DevNavBar />
       <Outlet />
     </div>
   );
+}
+
+function DevNavBar() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/home">
+          Hem
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/search">
+          game
+        </NavLink>
+      </li>
+    </ul>
+  )
 }
 
