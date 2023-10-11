4fdad23f095db96d026945e8101dbaae0a931af0
Joar-Rinaldo-Roos <joar.rinaldo.roos@gmail.com>
2023-10-11 13:13:53 +0200

    cleaned a bit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { useEffect, useState } from "react";
-import { FormControl } from "react-bootstrap";
-import ApiHandler from "./ApiHandler";
+
 import { NavLink, Outlet } from "react-router-dom";
-import LandingPage from "./LandingPage";
-import HomePage from "./components/HomePage";
+
 
 const CLIENT_ID = "41a89822d42c452fb778e429576a972b";
 const CLIENT_SECRET = "40a6ddb0f73d480094f24bd837e3dfba";
--- src/router.js
+++ src/router.js
@@ -9,24 +9,7 @@ import LeaderBoard from "./LeaderBoard";
 async function LeaderBoardLoader() {
   let score;
   return score = 1
-  // try {
-  //   const response = await fetch('/api/game-data'); // Replace with your actual API endpoint
-  //   if (!response.ok) {
-  //     throw new Error('Failed to load game data');
-  //   }
 
-  //   // Parse the response JSON
-  //   const gameData = await response.json();
-
-  //   // Return the data that needs to be passed to the ScoreCounter component
-  //   return {
-  //     gameData,
-  //   };
-  // } catch (error) {
-  //   // Handle errors, e.g., log them or show an error message to the user
-  //   console.error('Error loading game data:', error);
-  //   throw error; // Rethrow the error to indicate that the loading failed
-  // }
 }
 
 
