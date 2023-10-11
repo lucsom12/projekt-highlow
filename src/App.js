b9b5461cbe4f81fe058fa006b48704ba6c399245
Leo Stern <00leostern@gmail.com>
2023-10-11 14:40:30 +0200

    typ färdig landing-page, lagt till footer och lite så rebase landing page till main rebase landing page till main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from "./logo.svg";
+import "./BubblyButton.scss";
 import "./App.css";
+
 import React, { useEffect, useState } from "react";
 import { FormControl } from "react-bootstrap";
 import ApiHandler from "./ApiHandler";
@@ -13,8 +15,45 @@ const CLIENT_SECRET = "40a6ddb0f73d480094f24bd837e3dfba";
 export default function App() {
   return (
     <div className="container">
-      <DevNavBar />
-      <Outlet />
+      <div className="container col-12">
+        <DevNavBar />
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="container">
+      <footer className=" footer">
+        <ul class="nav col-10 offset-1 justify-content-center border-bottom rounded-circle pb-2 mb-3">
+          <li class="nav-item">
+            <a href="https://github.com/sternleo" target="_blank" className="nav-link px-2 text-body-secondary">
+              Leo Stern
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="https://github.com/lucsom12" target="_blank" className="nav-link px-2 text-body-secondary">
+              Luc Sommerland
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="https://github.com/Joar-Rinaldo-Roos" target="_blank" className="nav-link px-2 text-body-secondary">
+              Joar Rinaldo-Roos
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="https://github.com/AdamTegelberg" target="_blank" className="nav-link px-2 text-body-secondary">
+              Adam Tegelberg Hägnefors
+            </a>
+          </li>
+        </ul>
+        <p className="text-center text-body-secondary">
+          EDAF90 - webprogrammering
+        </p>
+      </footer>
     </div>
   );
 }
@@ -45,4 +84,3 @@ function DevNavBar() {
     </ul>
   )
 }
-
