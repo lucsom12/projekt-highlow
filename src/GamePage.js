d5f06e857752ca384bff8eeb4889f038b8cbc168
Luc Sommerland <lu1522so-s@student.lu.se>
2023-10-11 14:39:27 +0200

    Pop from track list

--- src/GamePage.js
+++ src/GamePage.js
@@ -2,11 +2,15 @@ import TrackDisplay from "./TrackDisplay";
 import React, { useState } from "react";
 
 function GamePage({ tracks }) {
+    let trackList = tracks
+    let trackIndex = []
     const [score, setScore] = useState(0);
     const [hiScore, setHiScore] = useState(0);
     const [showTrackPopularity, setShowTrackPopularity] = useState(false);
+    const [randTracks, setRandTracks] = useState(twoRandomTracks(trackList));
 
     function updateScore() {
+
         // setScore((object) => {
         //     const clone = object
         //     return clone + 1
@@ -15,6 +19,22 @@ function GamePage({ tracks }) {
         setScore(score + 1)
         setHiScore(Math.max(hiScore, score + 1))
         setShowTrackPopularity(true)
+        updateTrackList()
+    }
+
+    function updateTrackList() {
+        if (trackList.length <= 2) {
+            alert('congrats you got max score!')
+            console.log('done')
+        }
+        const remove = [trackIndex[0], trackIndex[1]];
+ 
+        console.log("pre: " + trackList.length)
+        for (let i = remove.length - 1; i >= 0; i--) trackList.splice(remove[i], 1);
+
+        console.log("post: " + trackList.length)
+
+        setRandTracks(twoRandomTracks(trackList))
     }
 
     function getRandomInt(max) {
@@ -30,15 +50,19 @@ function GamePage({ tracks }) {
         return [value1, value2]
     }
 
-    const [randInts, setRandInts] = useState(twoRandomInts(tracks.length));
+    function twoRandomTracks(tracks) {
+        const randIDs = twoRandomInts(tracks.length)
+        return [tracks[randIDs[0]], tracks[randIDs[1]]]
+    }
+
 
     return (
         <div className='container'>
             <div className="row d-flex justify-content-center align-items-center">
                 <p>Highest Score: {hiScore}</p>
                 <p>Score: {score}</p>
-                <TrackDisplay track={tracks[randInts[0]]} length={tracks.length} scoreFunction={updateScore} showPopularity={showTrackPopularity}/>
-                <TrackDisplay track={tracks[randInts[1]]} length={tracks.length} scoreFunction={updateScore} showPopularity={showTrackPopularity}/>
+                <TrackDisplay track={randTracks[0]} length={trackList.length} scoreFunction={updateScore} showPopularity={showTrackPopularity}/>
+                <TrackDisplay track={randTracks[1]} length={trackList.length} scoreFunction={updateScore} showPopularity={showTrackPopularity}/>
             </div>
         </div>
     )
