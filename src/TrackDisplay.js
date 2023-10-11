219825f36323297f7059f6a29eb7a0595f9e7987
Luc Sommerland <lu1522so-s@student.lu.se>
2023-10-11 11:41:55 +0200

    On click function

--- src/TrackDisplay.js
+++ src/TrackDisplay.js
@@ -1,12 +1,15 @@
 function TrackDisplay({ track, length }) {
+    function handleClick() {
+        console.log("rumpa!")
+        // alert('hello')
+    }
 
     if (length > 0) {
         return (
-            <div className="col-4">
+            <button type="button" onClick={() => handleClick()} className="btn btn-primary col-4">
                 < img src={track.album.images[0].url} style={{outline: "thick solid white", boxShadow: "16px 16px #191414"}}alt="" width="256" height="256"/>
                 <p>{track.name}</p>
-            </div>
-
+            </button>
         )
     }
 
