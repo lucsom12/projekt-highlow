4b2f3216cf60b8b8c76b28c4eb4243abb94783e6
Joar-Rinaldo-Roos <joar.rinaldo.roos@gmail.com>
2023-10-13 17:54:11 +0200

    made leaderboard mock again cuz firebase is cringe, and started work on form

--- src/LeaderBoard.js
+++ src/LeaderBoard.js
@@ -5,36 +5,38 @@ import { useEffect, useState } from "react";
 function LeaderBoard() {
 
     const [players, setPlayers] = useState([])
-    let mockPlayers = [
-        { name: 'adam', score: 200 },
-        { name: 'leo', score: 100 },
-        { name: 'luc', score: 300 },
-        { name: 'joar', score: 33 }
-    ];
     useEffect(() => {
-        async function fetchData() {
-            const db = getFirestore();
-            const scoresCollection = collection(db, "LeaderBoard2");
+        let mockPlayers = [
+            { name: 'adam', score: 200 },
+            { name: 'leo', score: 100 },
+            { name: 'luc', score: 300 },
+            { name: 'joar', score: 33 }
+        ];
+        setPlayers(mockPlayers)
 
-            console.log("fetching data");
-            try {
-                const querySnapshot = await getDocs(scoresCollection);
-                const data = []
-                querySnapshot.forEach((doc) => {
-                    const { playerName, score } = doc.data();
-                    data.push({ playerName, score });
-                });
+        //     async function fetchData() {
+        //         const db = getFirestore();
+        //         const scoresCollection = collection(db, "LeaderBoard2");
 
-                // Sort the data by score in descending order
-                data.sort((a, b) => b.score - a.score);
-                setPlayers(data);
-                console.log(data)
-            } catch (error) {
-                console.error("Error fetching data from Firebase: ", error);
-            }
-        }
+        //         console.log("fetching data");
+        //         try {
+        //             const querySnapshot = await getDocs(scoresCollection);
+        //             const data = []
+        //             querySnapshot.forEach((doc) => {
+        //                 const { playerName, score } = doc.data();
+        //                 data.push({ playerName, score });
+        //             });
 
-        fetchData();
+        //             // Sort the data by score in descending order
+        //             data.sort((a, b) => b.score - a.score);
+        //             setPlayers(data);
+        //             console.log(data)
+        //         } catch (error) {
+        //             console.error("Error fetching data from Firebase: ", error);
+        //         }
+        //     }
+
+        //     fetchData();
     }, []);
 
 
@@ -46,6 +48,7 @@ function LeaderBoard() {
                 {players.map((player, index) => (
                     <Entry key={player.name} name={player.playerName} score={player.score} />
                 ))}
+                <highscoreForm />
             </ul>
         </div>
     )
@@ -65,6 +68,40 @@ function compare(a, b) {
     }
     return 0;
 }
-
-
 export default LeaderBoard
+
+function highscoreForm() {
+    return (
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="recipient-name" class="col-form-label">Recipient:</label>
+                                <input type="text" class="form-control" id="recipient-name" />
+                            </div>
+                            <div class="form-group">
+                                <label for="message-text" class="col-form-label">Message:</label>
+                                <textarea class="form-control" id="message-text"></textarea>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Send message</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+
