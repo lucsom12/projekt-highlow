41a5e4dc4843592126b6db10e65d955077a05cd8
Leo Stern <00leostern@gmail.com>
2023-10-13 22:07:17 +0200

    mer styling

--- src/GamePage.js
+++ src/GamePage.js
@@ -2,47 +2,52 @@ import TrackDisplay from "./TrackDisplay";
 import React, { useEffect, useState } from "react";
 
 function GamePage({ tracks }) {
-    const [score, setScore] = useState(0);
-    const [hiScore, setHiScore] = useState(0);
+  const [score, setScore] = useState(0);
+  const [hiScore, setHiScore] = useState(0);
 
-    function updateScore() {
-        // setScore((object) => {
-        //     const clone = object
-        //     return clone + 1
-        //  })
-        //setScore(score + 1)
-        setScore(score + 1)
-        setHiScore(Math.max(hiScore, score + 1))
+  function updateScore() {
+    // setScore((object) => {
+    //     const clone = object
+    //     return clone + 1
+    //  })
+    //setScore(score + 1)
+    setScore(score + 1);
+    setHiScore(Math.max(hiScore, score + 1));
+  }
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  function twoRandomInts(maxLength) {
+    const value1 = getRandomInt(maxLength);
+    let value2 = getRandomInt(maxLength);
+    while (value2 === value1) {
+      value2 = getRandomInt(maxLength);
     }
+    return [value1, value2];
+  }
 
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-    }
+  const [randInts, setRandInts] = useState(twoRandomInts(tracks.length));
 
-    function twoRandomInts(maxLength) {
-        const value1 = getRandomInt(maxLength)
-        let value2 = getRandomInt(maxLength)
-        while (value2 === value1) {
-            value2 = getRandomInt(maxLength)
-        }
-        return [value1, value2]
-    }
-
-    const [randInts, setRandInts] = useState(twoRandomInts(tracks.length));
-
-    return (
-        <div className='container'>
-            <div className="row d-flex justify-content-center align-items-center">
-                <p>Highest Score: {hiScore}</p>
-                <p>Score: {score}</p>   
-                <div className="col-12">
-                    <TrackDisplay track={tracks[randInts[0]]} length={tracks.length} scoreFunction={updateScore} />
-                    <TrackDisplay track={tracks[randInts[1]]} length={tracks.length} scoreFunction={updateScore} />
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="row d-flex justify-content-between align-items-center">
+      <div className="col">
+        <TrackDisplay
+          track={tracks[randInts[0]]}
+          length={tracks.length}
+          scoreFunction={updateScore}
+        />
+      </div>
+      <div className="col">
+        <TrackDisplay
+          track={tracks[randInts[1]]}
+          length={tracks.length}
+          scoreFunction={updateScore}
+        />
+      </div>
+    </div>
+  );
 }
 
-
-export default GamePage
+export default GamePage;
